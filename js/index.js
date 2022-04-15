1270c979f549761e783e6da47eb3f9ecd3384b72
nseo87 <nick.seo@gmail.com>
2022-04-15 13:36:32 -0400

    Edits

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-$("button").click(function () {
-    $(".button-overlay").toggleClass("active");
+console.log("TESTING TESTING TESTING");
+
+/* DISABLED 
+// $('button').hover(function(){
+//     $(this).css({"border":'6px solid #0d1321', "background-color": '#F2AF29'});
+// $('a').hover(function(){
+//         $(this).css({"color": "#0d1321", "font-weight": "bold", "text-decoration": "none" });
+//     });
+// }); */
+
+$("button").hover(function(){
+    $(".button__Link").addClass("hover__ButtonText");
+    $("button").addClass("hover__State");
+});
+
+$("button").mouseleave(function(){
+    $(".button__Link").removeClass("hover__ButtonText");
+    $("button").removeClass("hover__State");
 });
